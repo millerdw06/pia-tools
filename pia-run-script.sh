a5e1b4b45313bea805c8d82e28c987312354eec2
Dave <millerdw06@gmail.com>
2015-08-26 12:27:39 -0600

    to much to list

--- pia-run-script.sh
+++ pia-run-script.sh
@@ -5,74 +5,105 @@
 
 args=$@
 
-SERVER='CA_Toronto'
+#SERVER='CA_Toronto'
+SERVER='Netherlands'
 
+_start_transmission(){
+    echo -n "Starting Transmission..."
+    if [[ -z $(pidof transmission-daemon) ]]; then
+        sudo -u dave transmission-daemon -g /home/dave/.config/transmission/ & > /dev/null 2>&1
+        while [[ -z $(pidof transmission-daemon)  ]]
+            do
+                sleep 1
+            done
+            echo "done"
+    fi
 
-_checkconn(){
-    if [[ -n $(systemctl status pia@$SERVER | grep "running")  ]]; then
-		echo 1
+    if [[ -z $(pgrep killswitch.sh) ]]; then
+        echo -n "Starting Killswitch..."
+        killswitch.sh & > /dev/null 2>&1
+        echo "done"
     else
-        echo 0
+        echo "Killswitch is already up."
     fi
-}  
-_startconn() {
-    if !( systemctl start pia@$SERVER ); then
-        exit 1
-    fi
-    sleep 4
 }
-_stopconn(){
-    if !( systemctl stop pia@$SERVER ); then
-        exit 1
+_stop_transmission(){
+    echo -n "Stopping Transmission..."
+    if [[ -n $(pidof transmission-daemon) ]]; then
+        while [[ -n $(pidof transmission-daemon) ]]
+        do
+            kill -9 $(pidof transmission-daemon)
+        done
     fi
-    sleep 3
+    echo "done"
+    if [[ -n $(pgrep killswitch.sh) ]]; then
+        echo -n "Stopping Killswitch..."
+        kill -9 $(pgrep killswitch.sh)
+        while [[ -n $(pgrep killswitch.sh) ]]
+        do
+            sleep 1
+        done
+        echo "done"
+    fi
 }
-_resetconn(){
-    if !( systemctl restart pia@$SERVER ); then
-        exit 1
-    fi
-    sleep 4
+_start_conn() {
+    echo -n "Starting VPN - $SERVER..."
+    systemctl start pia@$SERVER
+    while [[ -z "$(ip link | grep tun0)" ]]
+    do
+        sleep 1
+    done
+    echo "done"
+}
+_stop_conn(){
+    echo -n "Stopping VPN - $SERVER..."
+    systemctl stop pia@$SERVER
+    while [[ -n $(systemctl status pia@$SERVER | grep "Active: active (running)") ]]
+    do
+        sleep 1
+    done
+    echo "done"
+}
+_reset_conn(){
+    _stop_conn
+    _start_conn
 }
 #----------------------------------
-_conn(){
-	if [ `_checkconn` == '0' ]; then
-		echo "Connecting to $SERVER"
-		_startconn
-    fi
-    _status
+conn(){
+    _start_conn
+    _start_transmission
+    pia-tools -r
+    status
 }
-_disconn(){
-	if [ `_checkconn` == '1' ]; then
-		echo "Disconnecting from $SERVER"
-		_stopconn
-    fi
-	_status
-	
-	
+disconn(){
+    _stop_conn && systemctl restart iptables > /dev/null 2>&1 && _stop_transmission && status
 }
-_reset(){
-	_resetconn
-	_status
-}
-_status() {
-	
-	if [ `_checkconn` == '1' ]; then
-		echo "VPN - $SERVER: Up"
+
+status(){
+    echo " "
+	echo "PIA Status"
+	if [[ -n $(systemctl status pia@$SERVER | grep "Active: active (running)") ]]; then
+		echo "VPN...............Up - $SERVER"
 	else
-		echo "VPN - $SERVER: Down"
+		echo "VPN...............Down"
     fi
-    if [ -z `pidof transmission-daemon` ]; then
-        echo "Transmission: Down"
+    if [[ -n $(pgrep killswitch.sh) ]]; then
+        echo "Killswitch........Up - $(pgrep killswitch.sh)"
     else
-        echo "Transmission: Up: `pidof transmission-daemon`"
+        echo "Killswitch........Down"
+    fi
+    if [[ -n $(pidof transmission-daemon) ]]; then
+        echo "Transmission......Up - $(pidof transmission-daemon)"
+        transmission-remote -pt
+    else
+        echo "Transmission......Down"
     fi
 }
-_help(){
+help(){
 	echo "-c - connect"
 	echo "-d - disconnect"
 	echo "-r - reset connection"
 	echo "-s - connection status"
-
 }
 
 if [ "$EUID" -ne 0 ]
@@ -81,31 +112,32 @@ if [ "$EUID" -ne 0 ]
 fi
 
 case $args in
-	-c )   
-		_conn 
+	-c )
+		conn
 		;;
-
-	-d )      
-		_disconn	
+	-d )
+    status
+		disconn	
 		;;
-     
-	-r )      
-		_reset	
+	-r )
+		reset	
 		;;
 	-s )
-		_status 
+		status 
 		;;
 	-h )
-		_help
+		help
 		;;
-	 * )
-		_status 
+    --trans-start )
+        _start_transmission
+        ;;
+    --trans-stop )
+        _stop_transmission
+        ;;
+    * )
+		status
 		;;
 esac
 
-
 exit 0
-
-
-
 
